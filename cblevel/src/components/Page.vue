70de985ff1134a5018b9398d19cba7d61a03446b
Steve Yen <steve.yen@gmail.com>
2016-10-23 22:25:12 -0700

    started search method

--- cblevel/src/components/Page.vue
+++ cblevel/src/components/Page.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div v-if="page">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        page <i>{{$route.params.page_id}}</i>
+        page <i>{{page_id}}</i>
         <small>
-          {{pages[$route.params.page_id].description}}
+          {{page && page.description}}
         </small>
       </h1>
     </section>
@@ -14,25 +14,46 @@
     <section class="content">
       <!-- Your Page Content Here -->
 
-      <form action="#" method="get" class="sidebar-form">
+      <div>
         <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search..."/>
+          <input type="text" name="q"
+                 class="form-control" placeholder="Search..."/>
           <span class="input-group-btn">
             <button type="submit" name="search"
-                    class="btn btn-flat">
+                    class="btn btn-flat"
+                    v-on:click="search">
               <i class="fa fa-search"></i>
             </button>
           </span>
         </div>
-      </form>
+      </div>
     </section>
+  </div>
+
+  <div v-else>
+    <div class="alert alert-warning">
+      <h4><i class="icon fa fa-warning"></i> Error</h4>
+      unknown page "{{page_id}}"
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'page',
-  props: ['pages']
+  props: ['pages'],
+  computed: {
+    page_id () { return this.$route.params.page_id },
+    page () { return this.pages[this.$route.params.page_id] }
+  },
+  methods: {
+    search (event) {
+      // `this` inside methods points to the Vue instance
+      console.log('Hello ' + this.name + '!')
+      // `event` is the native DOM event
+      console.log(event.target.tagName)
+    }
+  }
 }
 </script>
 
