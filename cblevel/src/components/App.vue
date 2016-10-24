d7bfedcf58731a3b221e757a91044a20905444cb
Steve Yen <steve.yen@gmail.com>
2016-10-24 15:16:13 -0700

    added datasources header to sidebar

--- cblevel/src/components/App.vue
+++ cblevel/src/components/App.vue
@@ -25,6 +25,10 @@
   <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
+      <ul class="sidebar-menu">
+        <li class="header">DATA SOURCES</li>
+      </ul>
+
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel">
         <div class="pull-left image">
