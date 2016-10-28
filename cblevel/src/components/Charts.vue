f62431f3147a42de4fb8cf2a5fe0ff1200dfb297
Steve Yen <steve.yen@gmail.com>
2016-10-28 08:41:12 -0700

    chartjs test

--- cblevel/src/components/Charts.vue
+++ cblevel/src/components/Charts.vue
@@ -1,25 +1,59 @@
 <template>
-  <div>
-    <div class="box">
-      <div class="chart"></div>
-    </div>
+  <div class="box">
+    <canvas></canvas>
   </div>
 </template>
 
 <script>
-import colorbrewer from 'colorbrewer'
+import chartjs from 'chart.js'
 
-// (_optional_) define color function or array for bubbles: [ColorBrewer](http://colorbrewer2.org/)
-// .colors(colorbrewer.RdYlGn[9])
-console.log(colorbrewer)
+console.log(chartjs)
 
-function makeCharts () {
+function makeCharts (ctx) {
+  var myChart = new chartjs.Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  })
+  console.log(myChart)
 }
 
 export default {
   name: 'charts',
   mounted () {
-    makeCharts()
+    console.log(this.$el)
+    makeCharts(this.$el.firstElementChild)
   }
 }
 </script>
