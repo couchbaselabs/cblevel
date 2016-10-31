401c4ab2fe5d898a94f036a5529e9bf0350b5efc
Steve Yen <steve.yen@gmail.com>
2016-10-31 14:08:43 -0700

    added some example searches

--- cblevel/src/components/Page.vue
+++ cblevel/src/components/Page.vue
@@ -99,6 +99,93 @@
               <pre>{{searchRequest}}</pre>
             </div>
           </div>
+
+          <div>
+            <div class="checkbox">
+              <label>
+                <input v-model="searchExamples" type="checkbox"/>
+                  Show example searches
+              </label>
+            </div>
+
+            <div v-if="searchExamples"
+                 class="searchExamples box">
+              <div class="box-body row">
+                <dl class="col-md-4">
+                  <dt>fuzzy</dt>
+                  <dd>{ "term": "<i>internet</i>",
+  "prefix_length": <i>5</i>,
+  "fuzziness": <i>1</i>,
+  "field": "<i>fieldName</i>" }</dd>
+                  <dt>term</dt>
+                  <dd>{ "term": "<i>hello</i>",
+  "field": "<i>fieldName</i>" }</dd>
+                  <dt>match</dt>
+                  <dd>{ "match": "<i>internet</i>",
+  "field": "<i>fieldName</i>",
+  "analyzer": "<i>analyzerName</i>",
+  "prefix": <i>5</i>,
+  "fuzziness": <i>1</i>,
+  "operator": OPERATOR }
+
+OPERATOR: <i>0</i> = OR, <i>1</i> = AND</dd>
+                  <dt>match phrase</dt>
+                  <dd>{ "match_phrase": "<i>lazy dog</i>",
+  "field": "<i>fieldName</i>",
+  "analyzer": "<i>analyzerName</i>" }</dd>
+                  <dt>boolean</dt>
+                  <dd>{ "must": { &hellip; },
+  "must_not": { &hellip; },
+  "should": { &hellip; } }</dd>
+                </dl>
+                <dl class="col-md-4">
+                  <dt>phrase</dt>
+                  <dd>{ "terms": [ "<i>lazy</i>", "<i>dog</i>" ],
+  "field": "<i>fieldName</i>" }</dd>
+                  <dt>conjuncts (AND)</dt>
+                  <dd>{ "conjuncts": { &hellip; } }
+
+<i>+the +lazy +dog</i></dd>
+                  <dt>disjuncts (OR)</dt>
+                  <dd>{ "disjuncts": { &hellip; },
+  "min": <i>0</i> }
+
+<i>the lazy dog</i></dd>
+                  <dt>query string</dt>
+                  <dd>{ "query": "<i>the lazy dog</i>" }</dd>
+                  <dt>numeric range</dt>
+                  <dd>{ "min": <i>10</i>,
+  "max": <i>99</i>,
+  "inclusive_min": <i>false</i>,
+  "inclusive_max": <i>false</i>,
+  "field": "<i>fieldName</i>" }</dd>
+                  <dt>date range</dt>
+                  <dd>{ "start": <i>2001-10-09T10:20:30-08:00</i>",
+  "end": "<i>2016-10-31</i>",
+  "inclusive_start": <i>false</i>,
+  "inclusive_end": <i>false</i>,
+  "field": "<i>fieldName</i>" }</dd>
+                </dl>
+                <dl class="col-md-4">
+                  <dt>prefix</dt>
+                  <dd>{ "prefix": "<i>inter</i>",
+  "field": "<i>fieldName</i>" }</dd>
+                  <dt>regexp</dt>
+                  <dd>{ "regexp": "<i>inter.+</i>",
+  "field": "<i>fieldName</i>" }</dd>
+                  <dt>wildcard</dt>
+                  <dd>{ "wildcard": "<i>inter*</i>",
+  "field": "<i>fieldName</i>" }</dd>
+                  <dt>match all</dt>
+                  <dd>{ "match_all": {} }</dd>
+                  <dt>match none</dt>
+                  <dd>{ "match_none": {} }</dd>
+                  <dt>doc ids</dt>
+                  <dd>{ "ids": [ "<i>docId1</i>", &hellip; ] }</dd>
+                </dl>
+              </div>
+            </div>
+          </div>
         </div>
       </form>
 
@@ -316,6 +403,7 @@ export default {
     return {
       searchInput: null, // Ephemeral model for the search input control.
       searchAdvanced: false,
+      searchExamples: false,
       searchSize: 10000,
       searchFrom: 0,
       searchFields: '*',
@@ -487,5 +575,15 @@ export default {
 }
 .searchAdvanced .form-control {
   margin-bottom: 10px;
+}
+
+.searchExamples {
+  margin-left: 2em;
+}
+.searchExamples dd {
+  margin-bottom: 1em;
+  font-family: monospace;
+  font-size: 9pt;
+  white-space: pre;
 }
 </style>
